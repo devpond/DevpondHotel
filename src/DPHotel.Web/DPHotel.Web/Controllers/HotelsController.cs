9f66ab867896ad1c486845d76ded09b2beafa759
Simon Preston <si.preston@gmail.com>
2016-04-14 16:13:50 +0100

    Rebuilt hotel views

--- src/DPHotel.Web/DPHotel.Web/Controllers/HotelsController.cs
+++ src/DPHotel.Web/DPHotel.Web/Controllers/HotelsController.cs
@@ -7,8 +7,6 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DPHotel.Web.Models;
-using System.IO;
-using DPHotel.Web.Business;
 
 namespace DPHotel.Web.Controllers
 {
@@ -50,6 +48,8 @@ namespace DPHotel.Web.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,DateCreated,LastUpdated,Name,City")] Hotel hotel)
         {
+            hotel.DateCreated = DateTime.UtcNow;
+            hotel.LastUpdated = DateTime.UtcNow;
             if (ModelState.IsValid)
             {
                 db.Hotels.Add(hotel);
@@ -69,9 +69,8 @@ namespace DPHotel.Web.Controllers
             }
             Hotel hotel = db.Hotels.Find(id);
             if (hotel == null)
-            {
                 return HttpNotFound();
-            }
+            
             return View(hotel);
         }
 
@@ -82,6 +81,7 @@ namespace DPHotel.Web.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,DateCreated,LastUpdated,Name,City")] Hotel hotel)
         {
+            hotel.LastUpdated = DateTime.UtcNow;
             if (ModelState.IsValid)
             {
                 db.Entry(hotel).State = EntityState.Modified;
@@ -124,92 +124,6 @@ namespace DPHotel.Web.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
-        }
-        public ActionResult AddImage(IEnumerable<HttpPostedFileBase> files, int? GalleryId, string description)
-        {
-            //TODO: Check file is an image.
-            var newImgs = new List<HotelImage>();
-            var fileTools = new FileHelper();
-            int firstId = 0;
-            var fileName = "";
-            if (files != null)
-            {
-                foreach (var file in files)
-                {
-                    var newImg = new HotelImage();
-                    var fileType = fileTools.GetExtension(file.ContentType);
-                    System.Drawing.Image img = null;
-
-                    if (file.ContentLength > 0)
-                    {
-                        var isUnique = false;
-                        while (!isUnique)
-                        {
-                            fileName = fileTools.CreateFilename(fileType);
-                            if (!db.HotelImages.Any(x => x.FileName == fileName))
-                            {
-                                isUnique = true;
-                            }
-                        }
-                        var path = Path.Combine(Server.MapPath("~/Assets/Images"), fileName);
-                        file.SaveAs(path);
-                        img = System.Drawing.Image.FromFile(path); //This could be an issue if the file does not save.
-                    }
-                    // set attributes for image record
-                    newImg.FileName = fileName;
-
-                    // remove this extra step once proper login retrieval is fixed.
-                    //if (User.Identity.GetUserId() != null)
-                    //{
-                    //    newImg.UploadedBy = User.Identity.GetUserId();
-                    //}
-
-                    newImg.DateCreated = DateTime.Now;
-                    //newImg.IsHidden = false;
-                    newImg.Description = description;
-                    if (img != null)
-                    {
-                        newImg.Width = img.Width;
-                        newImg.Height = img.Height;
-                        newImg.Orientation = img.Width > img.Height ? "Landscape" : "Portrait";
-                    }
-
-                    //ViewBag.LocationID = new SelectList(db.site_locations, "LocationID", "Location");
-                    //ViewBag.AddedBy = new SelectList(db.AspNetUsers, "Id", "UserName");
-                    //ViewBag.PermissionLevel = new SelectList(db.AspNetRoles, "Id", "Name");
-                    newImgs.Add(newImg);
-                    //if (editedSubject != null)
-                    //{
-                    //    newImg.subjects1.Add(editedSubject);
-                    //}
-                }
-
-                foreach (var img in newImgs)
-                {
-                    db.HotelImages.Add(img);
-                }
-                if (ModelState.IsValid)
-                {
-                    var id = db.SaveChanges();
-                    //var newImgId = (from a in db.images
-                    //    where a.Filename == fileName
-                    //    select a.ImageID).FirstOrDefault();
-                    //var selImage = db.images.SingleOrDefault(x => x.ImageID == newImgId);
-                    if (newImgs.Count == 1)
-                    {
-                        HotelImage i = db.HotelImages.FirstOrDefault(x => x.FileName == fileName);
-                        if (i != null)
-                        {
-                            firstId = i.Id;
-                        }
-                        return RedirectToAction("ViewImage", new { Id = firstId });
-                    }
-                    return RedirectToAction("ViewImage");
-                }
-
-                return View("Error");
-            }
-            return View("Error");
         }
     }
 }
